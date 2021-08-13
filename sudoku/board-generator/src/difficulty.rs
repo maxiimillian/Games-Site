190d815ae48b21fd8303afde39d76e4268d18543
maxiimillian <maxtbell07@gmail.com>
2021-08-12 23:51:27 -0400

    Finished menu with options for easy, medium, and hard

--- sudoku/board-generator/src/difficulty.rs
+++ sudoku/board-generator/src/difficulty.rs
@@ -3,30 +3,35 @@ pub mod difficulty {
     #[derive(Clone)]
     pub struct Difficulty {
         pub filled_squares_count: i32,
+        pub name: String,
     }
 
     impl Difficulty {
         pub fn EASY() -> Difficulty {
             Difficulty {
                 filled_squares_count: 50,
+                name: "easy".to_string(),
             }
         }
 
         pub fn MEDIUM() -> Difficulty {
             Difficulty {
                 filled_squares_count: 40,
+                name: "medium".to_string(),
             }
         }
 
         pub fn HARD() -> Difficulty {
             Difficulty {
                 filled_squares_count: 30,
+                name: "hard".to_string(),
             }
         }
 
         pub fn EXTREME() -> Difficulty {
             Difficulty {
                 filled_squares_count: 26,
+                name: "extreme".to_string(),
             }
         }
     }
